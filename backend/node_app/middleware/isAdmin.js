165a450799b55c5f639bfa183eb08be323a303dc
chamathHirushan <chamathg.21@cse.mrt.ac.lk>
2024-02-04 09:03:55 +0530

    did not check

---
+++ backend/node_app/middleware/isAdmin.js
@@ -0,0 +1,45 @@
+const chatController = require('../controllers/chat.js');
+
+const isAdmin = async (req, res, next) => {
+    try {
+        // Check if the user is logged in
+        if (!req.body.user) {
+            return res.status(401).send('Unauthorized. Please log in.');
+        }
+
+        // Check if the user has the 'admin' role
+        const chat = await chatController.getChat(req.body.id);
+        if (!chat.Admins.includes(req.body.user)) {
+            return res.status(403).send('Access forbidden. Only admin users are allowed.');
+        }
+
+        // User is an admin, proceed to the next middleware or route handler
+        next();
+    } catch (error) {
+        console.error('Error in isAdmin middleware:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+const isInitialAdmin = async (req, res, next) => {
+    try {
+        // Check if the user is logged in
+        if (!req.user) {
+            return res.status(401).send('Unauthorized. Please log in.');
+        }
+
+        // Check if the user has the 'admin' role
+        const chat = await chatController.getChat(req.body.id);
+        if (!(chat.groupInitialAdmin === req.user.id)) {
+            return res.status(403).send('Access forbidden. Only the initial admin is allowed.');
+        }
+
+        // User is the initial admin, proceed to the next middleware or route handler
+        next();
+    } catch (error) {
+        console.error('Error in isInitialAdmin middleware:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+module.exports = { isAdmin, isInitialAdmin };
